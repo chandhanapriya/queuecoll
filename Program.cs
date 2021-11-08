3d268c690698234e389dffadc8f059e6e6996db0
chandhanapriya T <92292189+chandhanapriya@users.noreply.github.com>
2021-11-08 18:44:33 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 
-namespace Demo_Odataservice
+
+namespace demo_queue_coll
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* Queue<int> MyQue = new Queue<int>();
+             MyQue.Enqueue(120);
+             MyQue.Enqueue(200);
+             MyQue.Enqueue(980);
+             Console.WriteLine("First element removed in queue{0}",MyQue.Count);
+             Console.WriteLine("type of queue{0}", MyQue.GetType());
+             MyArrayList.Add(24);
+             MyArrayList.Add("nandini");
+             MyArrayList.Add(24.4762);
+             MyArrayList.Add(true);
+             MyArrayList.Add("IO Exception");
+             foreach(var item in MyArrayList)*/
+
+            //SortedList<string,int>GameScore= 
+
+            Dictionary<string,string> pwddetails = new Dictionary<>();
+
+            pwddetails.Add("facebook", "%&@*");
+            pwddetails.Add("gmail", "$#%*%(%");
+            pwddetails.Add("outlook", "#^%)+");
+
+            foreach(var item in pwddetails)
+
+            Console.WriteLine("content of dictionary key is"
+
         }
     }
 }
